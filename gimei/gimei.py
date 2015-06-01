464f587f7eb67648b95a146a6a0ee992227e891a
nabetama <mao.nabeta@gmail.com>
2015-06-02 01:12:06 +0900

    Create Gimei class

--- gimei/gimei.py
+++ gimei/gimei.py
@@ -2,7 +2,17 @@
 import yaml
 
 
+MALE = 'male'
+FEMALE = 'female'
+GENDER = (MALE, FEMALE)
+
+
+def yaml_load(file_path, mode='r'):
+    f = open(file_path, mode)
+    return yaml.load(f)
+
+
+
 class Gimei(object):
-    def __init__(self, gender=None):
-        self.name = Name(gender)
-        self.address = Address()
+    NAMES = yaml_load('./data/names.yml')
+    ADDRESSES = yaml_load('./data/addresses.yml')
