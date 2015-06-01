fd6bbb6800f2d866627f65db0cab86f43379123e
nabetama <mao.nabeta@gmail.com>
2015-06-02 02:32:26 +0900

    gimei class.

--- gimei/gimei.py
+++ gimei/gimei.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import random
 import yaml
 
+
+from address import Address
+from name import Name
 
 MALE = 'male'
 FEMALE = 'female'
@@ -16,3 +20,17 @@ def yaml_load(file_path, mode='r'):
 class Gimei(object):
     NAMES = yaml_load('./data/names.yml')
     ADDRESSES = yaml_load('./data/addresses.yml')
+
+    def __init__(self, gender=None):
+        if gender is None:
+            gender = random.choice(GENDER)
+        self.gender = gender
+
+
+    @property
+    def name(self):
+        return Name(self.gender)
+
+    @property
+    def address(self):
+        return Address()
