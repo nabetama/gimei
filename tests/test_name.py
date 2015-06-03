162ed3ddb2a3a7798f7396c37d6a534dc103bbcf
nabetama <mao.nabeta@gmail.com>
2015-06-03 15:40:21 +0900

    Add Name test.

--- tests/test_name.py
+++ tests/test_name.py
@@ -1,2 +1,25 @@
 # coding: utf-8
+from gimei import Gimei
+from gimei import Name
 
+
+class TestName(object):
+    def test_kanji_name_in_data(self):
+        name = Gimei().name
+        assert Name.find_name_by_kanji(name.kanji)
+
+    def test_hiragana_name_in_data(self):
+        name = Gimei().name
+        assert Name.find_name_by_hiragana(name.hiragana)
+
+    def test_katakana_name_in_data(self):
+        name = Gimei().name
+        assert Name.find_name_by_katakana(name.katakana)
+
+    def test_create_name_from_gender(self):
+        name = Gimei('male').name
+        assert name.is_male
+
+    def test_create_name_from_gender(self):
+        name = Gimei('female').name
+        assert name.is_female
