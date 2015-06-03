0821f0812d83aae44ec79b96ba0074d19ca9f0fc
nabetama <mao.nabeta@gmail.com>
2015-06-03 15:45:53 +0900

    Add unit test.

--- tests/test_address.py
+++ tests/test_address.py
@@ -1,2 +1,16 @@
 # coding: utf-8
+from gimei import Gimei
+from gimei import Address
 
+class TestAddress(object):
+    def test_kanji_address_in_data(self):
+        address = Gimei().address
+        assert Address.find_address_by_kanji(address.kanji)
+
+    def test_hiragana_address_in_data(self):
+        address = Gimei().address
+        assert Address.find_address_by_hiragana(address.hiragana)
+
+    def test_katakana_address_in_data(self):
+        address = Gimei().address
+        assert Address.find_address_by_katakana(address.katakana)
